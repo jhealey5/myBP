9b720d75f572c49e68c8237d04a43eee031b66ee
John Healey <jhealey5@gmail.com>
2012-06-02 09:49:05 +0100

    Minor edits

--- includes/end.php
+++ includes/end.php
@@ -1,10 +1,10 @@
-  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="scripts/libs/jquery-1.7.2.min.js"><\/script>')</script>
+	<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="scripts/libs/jquery-1.7.2.min.js"><\/script>')</script>
 
-  <script src="scripts/plugins.js"></script>
-  <script src="scripts/script.js"></script>
-  <!-- end scripts -->
+	<script src="scripts/plugins.js"></script>
+	<script src="scripts/script.js"></script>
+	<!-- end scripts -->
   
 </body>
 </html>

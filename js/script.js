abc2c041b2ce4f0ff6c439bc1abbd2c548b1072e
John Healey <johnhealey@Administration.local>
2013-06-24 11:41:21 +0100

    Minor changes, added user agent classes

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 	// no-js check
 	$('html').removeClass('no-js').addClass('js');
 
-	
+	//browser detection
+	var doc = document.documentElement;
+	doc.setAttribute('data-useragent', navigator.userAgent);
   
 }); // end doc.ready
